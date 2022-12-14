270683c4a7c24fc452e6db697ea7de4a5fef743e
thomas Fran <thomasfran1935@icloud.com>
2022-12-15 00:57:08 +0100

    Dana content done

--- src/havenOfOblivion/Dana.jsx
+++ src/havenOfOblivion/Dana.jsx
@@ -1,4 +1,4 @@
-import BossHeader from '../bossHeader';
+import BossHeader from "../bossHeader";
 
 const Dana = () => {
   let teamComp = {
@@ -18,13 +18,42 @@ const Dana = () => {
         img={"https://i.imgur.com/kI0pQ4u.png"}
       />
       <div className="ccBossConcent">
-        <h3>{localStorage.getItem('language') === 'EN' ? "Boss mechanics :" : 'Méchanique du boss:'}</h3>
+        <h3>
+          {localStorage.getItem("language") === "EN"
+            ? "Boss mechanics :"
+            : "Méchanique du boss:"}
+        </h3>
         <ul className="ccBossMecha">
-        {localStorage.getItem("language") === "EN" ? <li></li> : <li></li>}
+          {localStorage.getItem("language") === "EN" ? (
+            <li>
+              Dana casts a flame 'debuff', this 'debuff' can be used to remove
+              her Ice Coffin Root, it is generally optimal to have the team
+              standing in close proximity so whoever has the flame debuff can
+              remove the Root.
+            </li>
+          ) : (
+            <li>
+              Dana appliquera un debuff qui ressemble a une flame, ce debuff
+              permettre de retirer the debuff 'Ice Coffin Root', il est donc
+              conseillé de rester proche les un des autres de manière à ce que
+              celui qui reçoit le debuff, puisse retirer le 'Ice Coffin Root'.
+            </li>
+          )}
+          {localStorage.getItem("language") === "EN" ? (
+            <li>
+              However, it is easier to simply CC Chain her after 75% HP, so she
+              doesn't cast the skill at all.
+            </li>
+          ) : (
+            <li>
+              Cependant, il est plus simple d'enchainer les CC à partir de 75%
+              HP afin de l'empêcher d'utiliser son 'Ice Coffin Root'.
+            </li>
+          )}
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Dana;
