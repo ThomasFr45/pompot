82bb132677016f7cead8b38eb17629e99afb3229
thomas Fran <thomasfran1935@icloud.com>
2022-12-15 01:17:43 +0100

    Phoenix content done

--- src/havenOfOblivion/Phoenix.jsx
+++ src/havenOfOblivion/Phoenix.jsx
@@ -20,7 +20,8 @@ const Phoenix = () => {
       <div className="ccBossConcent">
         <h3>{localStorage.getItem('language') === 'EN' ? "Boss mechanics :" : 'Méchanique du boss:'}</h3>
         <ul className="ccBossMecha">
-        {localStorage.getItem("language") === "EN" ? <li></li> : <li></li>}
+        {localStorage.getItem("language") === "EN" ? <li>Before attacking Phoenix, it is advised to kill the monsters, as it applies a debuff that will remove 50K HP every time you hit a target. This debuff can be removed by the healer.</li> : <li>Avant de s'attaquer à Phoenix, il est conseillé de tuer les monstres, puisqu'il applique un debuff qui vous retirera 50K HP à chaque fois que vous tapperez une cible. Ce debuff peut être retiré par le healer.</li>}
+        {localStorage.getItem("language") === "EN" ? <li>As for the CC, i'd advise you to fear and then KD at 50% hp.</li> : <li>En ce qui concerne les CC, il est conseillé d'enchainer le fear et KD vers 50% hp.</li>}
         </ul>
       </div>
     </div>
