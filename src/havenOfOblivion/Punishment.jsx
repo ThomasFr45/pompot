34ac0b642cd64d35a50a7827360e93f4f847e638
thomas Fran <thomasfran1935@icloud.com>
2022-12-15 01:20:15 +0100

    Punishment content done

--- src/havenOfOblivion/Punishment.jsx
+++ src/havenOfOblivion/Punishment.jsx
@@ -20,7 +20,10 @@ const Punishment = () => {
       <div className="ccBossConcent">
         <h3>{localStorage.getItem('language') === 'EN' ? "Boss mechanics :" : 'Méchanique du boss:'}</h3>
         <ul className="ccBossMecha">
-        {localStorage.getItem("language") === "EN" ? <li></li> : <li></li>}
+        {localStorage.getItem("language") === "EN" ? <li>First, it is recommended to ask the Executioner to swap to a strike weapon for the BP.</li> : <li>Premièrement, il est conseillé de demander a l'Executioner du groupe de passer en strike pour aider avec la BP.</li>}
+        {localStorage.getItem("language") === "EN" ? <li>Then, when Punishment summons Volcano spots that deal dmg, the Berserker will have to push them to limit the damage the party will receive.</li> : <li>Ensuite, quand Punishment fera spawn des tas de sable, il faudra que le Berserker les pousses histoire de limiter les dégats reçus.</li>}
+        {localStorage.getItem("language") === "EN" ? <li>You should fear, stun, and KD to 50% HP.</li> : <li>Il est conseiller le de fear, stun et KD vers 50% HP.</li>}
+        {localStorage.getItem("language") === "EN" ? <li>If the boss is not killed fast enough, it is possible that he will summon crystals that will reduce the damage he will receive.</li> : <li>Si le boss n'est pas tuer assez vite, il est possible qu'il invoque des cristaux réduisant les dégats qu'il recevra.</li>}
         </ul>
       </div>
     </div>
